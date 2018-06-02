467f487b926e8de6d1ec748bf80dfa7305f8e37f
M-Onitsuka <onitsuka@jsk.imi.i.u-tokyo.ac.jp>
2018-06-03 00:36:22 +0900

    rm typo

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   // start from 1 to ignore script name; argv[0] will be a name of processing file.
-  cout << "Hello " << arcv[1] << "!" << endl;
+  cout << "Hello " << argv[1] << "!" << endl;
   // for (int i = 1; i < argc; i++) {
   //   printf ("argv[%i]: %s\n", i, argv[i]);
   //}
